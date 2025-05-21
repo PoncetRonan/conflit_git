81b5492ff2ed3302e503211dba82b6398b397b96
Diletta Ciardo <dilettaciardo@yahoo.it>
2025-05-21 12:34:51 +0200

    test pull request

--- conflit_new.py
+++ conflit_new.py
@@ -4,3 +4,5 @@ def votre_nom(nom : str):
 votre_nom('Jérôme')
 print('hi! i am happy to create problems')
 print("I am always 10 times happier to create problems than you")
+
+print('want to test a pull request')
